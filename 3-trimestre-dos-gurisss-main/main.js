40d2c6654e026f10948aa8f779fb402c16445b82
ROSA <126817382+GUUH0105@users.noreply.github.com>
2023-11-01 13:50:22 -0300

    01/11

--- 3-trimestre-dos-gurisss-main/main.js
+++ 3-trimestre-dos-gurisss-main/main.js
@@ -1,2 +1,13 @@
-document.querySelector('.tecla_pom');
-document.querySelector('#som_tecla_pom').play();
+function tocaSomPom(){
+    document.querySelector('#som_tecla_pom').play();
+}
+const listaDeTeclas = document.querySelectorAll('.tecla');
+
+let contador = 0;
+
+while(contador <listaDeTeclas.length){
+    listaDeTeclas[0].onclick = tocaSomPom;
+    contador = contador + 1;
+
+    console.log(contador);
+}
